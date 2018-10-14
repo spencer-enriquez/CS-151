960afad47ab48d97a2091d02f1428e683b2d2be8
Spencer Enriquez <spencer.enriquez@sjsu.edu>
2018-10-14 12:59:51 -0700

    Added "Apply Changes Button", but needs to be able to remove old graph and repaint new one.

--- hw4/FrameLayout.java
+++ hw4/FrameLayout.java
@@ -2,25 +2,30 @@ import java.awt.*;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 
 public class FrameLayout {
+	private ArrayList<Category> cat = new ArrayList<Category>();
+	GridBagConstraints gbc = new GridBagConstraints();
+	JPanel textPanel;
+	JPanel graphPanel;
+	
+	
 	public FrameLayout() {
-		ArrayList<Category> cat = new ArrayList<Category>();
-		cat.add(new Category("iPhone XS", 100));
-		cat.add(new Category("Samsung S9", 100));
-		cat.add(new Category("Google Pixel 3", 100));
-		cat.add(new Category("Nokie 74", 100));
-		cat.add(new Category("Flip Phone", 100));
-		cat.add(new Category("Other", 100));
+		gbc.insets = new Insets(5,5,5,5);
+
+		cat.add(new Category("iPhone XS", 100, new JTextField(5)));
+		cat.add(new Category("Samsung S9", 100,  new JTextField(5)));
+		cat.add(new Category("Google Pixel 3", 100,  new JTextField(5)));
+		cat.add(new Category("Nokie 74", 100,  new JTextField(5)));
+		cat.add(new Category("Flip Phone", 100,  new JTextField(5)));
+		cat.add(new Category("Other", 100,  new JTextField(5)));
 
 		JPanel textPanel = new JPanel();
 		 textPanel.setLayout(new BorderLayout());
 		 textPanel.add(new JLabel("Number Input"), BorderLayout.NORTH);
 		 JPanel gridText = new JPanel(new GridBagLayout());
-		 textPanel.add(gridText, BorderLayout.CENTER);
-		 GridBagConstraints gbc = new GridBagConstraints();
-		 gbc.insets = new Insets(5,5,5,5);
-		 
+		 textPanel.add(gridText, BorderLayout.CENTER);		 
 		 for (int i = 0; i < cat.size(); i++) {
 			 gbc.gridx = 0;
 			 gbc.gridy = i;
@@ -30,28 +35,33 @@ public class FrameLayout {
 		 for (int i = 0; i < cat.size(); i++) {
 			 gbc.gridx = 1;
 			 gbc.gridy = i;
-			 gridText.add(new JTextField("00"), gbc);
+			 gridText.add(cat.get(i).getTextField(), gbc);
 		 }
+		 JButton changes = new JButton("Apply Changes");
+		 textPanel.add(changes, BorderLayout.SOUTH);
+				 
 		 
 		 JPanel graphPanel = new JPanel();
 		 graphPanel.setLayout(new BorderLayout());
 		 graphPanel.add(new JLabel("Graph Representation"), BorderLayout.NORTH);
-		 JPanel gridGraph = new JPanel(new GridBagLayout());
+		 JPanel gridGraph = paintGraph();									// makes and paints graph
 		 graphPanel.add(gridGraph, BorderLayout.CENTER);
 		 
-		 for (int i = 0; i < cat.size(); i++) {
-			 gbc.gridx = 0;
-			 gbc.gridy = i;
-			 gridGraph.add(new JLabel(cat.get(i).getName()), gbc);
-		 }
 		 
-		 for(int i = 0; i < cat.size(); i++) {
-			 gbc.gridx = 1;
-			 gbc.gridy = i;
-			 Bar b = new Bar(cat.get(i).getValue());
-			 JLabel rect = new JLabel(b);
-			 gridGraph.add(rect, gbc);
-		 }
+		 
+		 
+		 changes.addActionListener(event ->
+		 {
+			 for (int i = 0; i < cat.size(); i++) {
+				String text = cat.get(i).getTextField().getText();
+				if (!text.equals(""))
+					cat.get(i).setValue(Integer.parseInt(text));
+			 }
+			 graphPanel.updateUI();
+		 });
+		 
+		 
+		 
 		 
 		 
 		 JFrame frame = new JFrame();
@@ -64,6 +74,31 @@ public class FrameLayout {
 
 }
 	
+	public JPanel paintGraph() {
+		JPanel panel = new JPanel(new GridBagLayout());
+		for (int i = 0; i < cat.size(); i++) {
+			 gbc.gridx = 0;
+			 gbc.gridy = i;
+			 gbc.anchor = gbc.LINE_START;
+			 panel.add(new JLabel(cat.get(i).getName()), gbc);
+		 }
+		
+		for(int i = 0; i < cat.size(); i++) {
+			 gbc.gridx = 1;
+			 gbc.gridy = i;
+			 Bar b = new Bar(cat.get(i).getValue());
+			 JLabel rect = new JLabel(b);
+			 panel.add(rect, gbc);
+		 }
+		return panel;
+	}
+/*	
+	public void changeGraph() {
+		JPanel graph = paintGraph();									// makes and paints graph
+		graphPanel.add(graph, BorderLayout.CENTER);
+		ChangeEvent event = new ChangeEvent(this);
+	}
+*/
 	public static void main(String[] args)
 	{
 		FrameLayout me = new FrameLayout();
